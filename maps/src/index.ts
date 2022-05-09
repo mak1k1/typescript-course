1422bcf170e545a7ced42c4ee7fe48cca6eadd48
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-09 13:13:59 +0200

    Connected with google maps api

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hi There')
+/// <reference types="@types/google.maps" />
+import { User } from './User'
+import { Company } from './Company'
+
+const user = new User()
+
+console.log(user)
+
+const company = new Company()
+
+console.log(company)
+
+const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+})
