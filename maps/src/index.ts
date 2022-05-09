324f682a5682390db415fe2b503f785be8082f6c
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-09 13:19:57 +0200

    Created CustomMap class to handle googleapi map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,19 +1,4 @@
-/// <reference types="@types/google.maps" />
-import { User } from './User'
-import { Company } from './Company'
+import { CustomMap } from './CustomMap'
 
-const user = new User()
+new CustomMap('map')
 
-console.log(user)
-
-const company = new Company()
-
-console.log(company)
-
-const map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-})
