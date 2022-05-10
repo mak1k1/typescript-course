620db5da5e91ad523bcfa015007627a8455fb954
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-10 22:44:07 +0200

    Refactored using abstract generic class

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,18 +1,16 @@
 import fs from 'fs'
-import { dateStringToDate } from './utils'
 import { MatchResult } from './MatchResult'
-import internal from 'stream'
 
-type MatchData = [Date, string, string, number, number, MatchResult, string]
-
-export class CsvFileReader {
-  data: MatchData[] = []
+export abstract class CsvFileReader<T> {
+  data: T[] = []
   filename: string
 
   constructor(filename: string) {
     this.filename = filename
   }
 
+  abstract mapRow(row: string[]): T
+  
   read(): void {
     this.data = fs
       .readFileSync(this.filename, {
@@ -22,20 +20,7 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',')
       })
-      .map(
-        (
-          row: string[]
-        ): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6],
-          ]
-        }
-      )
+      .map(this.mapRow)
   }
+
 }
