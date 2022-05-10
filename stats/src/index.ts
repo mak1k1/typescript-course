b1310797b6dc7e1a5ca546b4a3d2c9bbef336434
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-10 22:12:50 +0200

    created class to handle file reading from csv

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,24 +1,23 @@
-import fs from 'fs'
+import { CsvFileReader } from "./CsvFileReader"
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',')
-  })
+const reader = new CsvFileReader('football.csv')
+reader.read()
 
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
 
-  let manUnitedWins = 0
+let manUnitedWins = 0
 
-  for(let match of matches) {
-    if(match[1] === 'Man United' && match[5] === 'H') {
-        manUnitedWins++
-    }
-    if(match[2] === 'Man United' && match[5] === 'A') {
-        manUnitedWins++
-    }
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++
   }
+  if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++
+  }
+}
 
-  console.log(`Man United won ${manUnitedWins} games`)
+console.log(`Man United won ${manUnitedWins} games`)
