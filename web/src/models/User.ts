3abebbf4cf7d8680f471462cb81ddde2762f0c8c
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-13 16:14:03 +0200

    Refactored events into Eventing class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
 
 interface UserProps {
   id?: number
@@ -6,12 +7,10 @@ interface UserProps {
   age?: number
 }
 
-type Callback = () => void
-
 export class User {
-  private events: { [key: string]: Callback[] } = {}
-
-  constructor(private data: UserProps) {}
+	public events: Eventing = new Eventing()
+	
+	constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
     return this.data[propName]
@@ -19,24 +18,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update)
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || []
-    handlers.push(callback)
-    this.events[eventName] = handlers
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName]
-
-    if (!handlers || handlers.length === 0) {
-      return
-    }
-
-    handlers.forEach((callback) => {
-      callback()
-    })
   }
 
   fetch(): void {
@@ -51,9 +32,8 @@ export class User {
     const id = this.get('id')
     if (id) {
       axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      axios.post('http://localhost:3000/users', this.data)
     }
-		else {
-			axios.post('http://localhost:3000/users', this.data)
-		}
   }
 }
