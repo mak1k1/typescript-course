ed95451b191a4c39e617fac53d60d53f4e9fa66f
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-13 19:56:43 +0200

    Refactored attributes into Attributes class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,3 +1,4 @@
+import { Attributes } from './Attributes'
 import { Eventing } from './Eventing'
 import { Sync } from './Sync'
 
@@ -12,14 +13,9 @@ const rootUrl = 'http://localhost:3000/users'
 export class User {
   public events: Eventing = new Eventing()
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
+	public attributes: Attributes<UserProps>
 
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName]
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update)
-  }
+	constructor(attrs: UserProps){
+		this.attributes = new Attributes<UserProps>(attrs)
+	}
 }
