b247d4d03e9905c50d0093c138a99682b874c165
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-13 16:01:47 +0200

    Added fetch and save methods

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,27 +1,2 @@
 import { User } from './models/User'
 
-const user = new User({name: 'modemo', age: 24})
-
-console.log(user.get('name'))
-
-user.set({name: 'makiki'})
-
-console.log(user.get('name'))
-console.log(user.get('age'))
-
-user.on('lmao', () => {
-    console.log('it wrks')
-})
-
-
-user.on('lmao', () => {
-    console.log('it wrks agen')
-})
-
-user.on('rofl', () => {
-    console.log('it doesn wrk')
-})
-
-console.log(user)
-
-user.trigger('lmao')
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,7 @@
+import axios, { AxiosResponse } from 'axios'
+
 interface UserProps {
+  id?: number
   name?: string
   age?: number
 }
@@ -19,20 +22,38 @@ export class User {
   }
 
   on(eventName: string, callback: Callback): void {
-		const handlers = this.events[eventName] || []
+    const handlers = this.events[eventName] || []
     handlers.push(callback)
-		this.events[eventName] = handlers
+    this.events[eventName] = handlers
   }
 
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName]
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName]
 
-		if(!handlers || handlers.length === 0) {
-			return
-		} 
+    if (!handlers || handlers.length === 0) {
+      return
+    }
 
-		handlers.forEach(callback => {
-			callback()
-		})
-	}
+    handlers.forEach((callback) => {
+      callback()
+    })
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data)
+      })
+  }
+
+  save(): void {
+    const id = this.get('id')
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    }
+		else {
+			axios.post('http://localhost:3000/users', this.data)
+		}
+  }
 }
