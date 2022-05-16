9211262a68985f71ab5a843b53f30a729467f4d4
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-16 23:07:41 +0200

    Created UserForm view

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,10 +1,4 @@
-import { Collection } from './models/Collection'
-import { User, UserProps } from './models/User'
+import { UserForm } from './views/UserForm'
 
-const collection = User.buildUserCollection()
-
-collection.on('change', () => {
-  console.log(collection)
-})
-
-collection.fetch()
+const userForm = new UserForm(document.getElementById('root'))
+userForm.render()
