6f468d6f1583be88e53aa746b0ad52b5441bccf4
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-19 15:54:11 +0200

    Connected the UserForm with User model

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,29 +1,58 @@
+import { User } from '../models/User'
+
 export class UserForm {
-	constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel()
+  }
 
-	eventsMap(): {[key: string]: () => void} {
-		return {
-			'click:button': this.onButtonClick,
-		}
-	}
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render()
+    })
+  }
 
-	onButtonClick(): void {
-		console.log('Hello there')
-	}
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+    }
+  }
 
-	template(): string {
-		return `
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge()
+  }
+
+  template(): string {
+    return `
 			<div>
 				<h1>User Form</h1>
+				<div>User name: ${this.model.get('name')}</div>
+				<div>User age: ${this.model.get('age')}</div>
 				<input />
+				<button>Click Me</button>
+				<button class="set-age">Set Random Age</button>
 			</div>
 		`
-	}
+  }
 
-	render(): void {
-		const templateElement = document.createElement('template')
-		templateElement.innerHTML = this.template()
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':')
 
-		this.parent.append(templateElement.content)
-	}
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey])
+      })
+    }
+  }
+
+  render(): void {
+		this.parent.innerHTML = ''
+		
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+
+    this.bindEvents(templateElement.content)
+
+    this.parent.append(templateElement.content)
+  }
 }
