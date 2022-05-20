43ca8819dc50a3925816fe8538380524f8fb9f42
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-20 13:30:29 +0200

    Created a collection view for users

--- web/src/views/UserShow.ts
+++ web/src/views/UserShow.ts
@@ -4,7 +4,7 @@ import { User, UserProps } from '../models/User'
 export class UserShow extends View<User, UserProps> {
 	template(): string {
 		return `
-			<h1>User Detail<h1/>
+			<h1>User Detail</h1>
 			<div>User Name: ${this.model.get('name')}</div>
 			<div>User Name: ${this.model.get('age')}</div>
 		`
