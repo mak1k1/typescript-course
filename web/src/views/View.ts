ea7f03d9b2bb9fc082af878ff802c8e2328f6e1d
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-20 13:10:05 +0200

    Implemented region mapping

--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -1,13 +1,19 @@
-import { Model } from "../models/Model"
+import { Model } from '../models/Model'
 
 export abstract class View<T extends Model<K>, K> {
+  regions: { [key: string]: Element } = {}
+
   constructor(public parent: Element, public model: T) {
     this.bindModel()
   }
 
   abstract template(): string
-	
+
   eventsMap(): { [key: string]: () => void } {
+    return {}
+  }
+
+	regionsMap(): {[key: string]: string} {
 		return {}
 	}
 
@@ -28,6 +34,17 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+	mapRegions(fragment: DocumentFragment): void {
+		const regionsMap = this.regionsMap()
+		for(let key in regionsMap) {
+			const selector = regionsMap[key]
+			const element = fragment.querySelector(selector)
+			if(element) {
+				this.regions[key] = element
+			}
+		}
+	}
+
   render(): void {
     this.parent.innerHTML = ''
 
@@ -35,6 +52,7 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template()
 
     this.bindEvents(templateElement.content)
+		this.mapRegions(templateElement.content)
 
     this.parent.append(templateElement.content)
   }
