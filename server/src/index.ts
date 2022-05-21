d2bc59fd0ca675f9773ce4b35154dd4651366f5c
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-21 21:58:10 +0200

    Created login get and post request

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
-import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express'
 import { router } from './routes/loginRoutes'
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(router)
 
 app.listen(3000, () => {
-	console.log('Listening on port 3000')
+  console.log('Listening on port 3000')
 })
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,9 +1,29 @@
 import { Router, Request, Response } from 'express'
+import bodyParser from 'body-parser'
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('hi there')
+router.get('/login', (req: Request, res: Response) => {
+  res.send(
+    `
+		<form>
+			<div>
+				<label>Email</label>
+				<input name="email" />
+			</div>
+			<div>
+				<label>Password</label>
+				<input name="password" type="password" />
+			</div>
+			<button>Submit</button>
+		</form>
+		`
+  )
+})
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body
+  res.send(email + password)
 })
 
 export { router }
