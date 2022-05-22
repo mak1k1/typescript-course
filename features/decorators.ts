2acf3ef565de87555a307a81622ac5d381a95e2d
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-22 16:34:23 +0200

    Class decorators, parameter decorators

--- features/decorators.ts
+++ features/decorators.ts
@@ -1,28 +1,49 @@
+@classDecorator
 class Boat {
-  color: string = 'red'
-
+	color: string = 'red'
+	
+	@testDecorator
   get formattedColor(): string {
     return `This boat's color is ${this.color}`
   }
 
-	@logError
-  pilot(): void {
-		throw new Error()
-    console.log('swish')
+  @logError('Oops, boat was sunk in ocean')
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if(speed === 'fast') {
+			console.log('swish')
+		}
+		else {
+			console.log('nothing')
+			throw new Error()
+		}
   }
 }
 
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value
-
-	desc.value = function () {
-		try {
-			method()
-		}
-		catch(e){
-			console.log('Oops, boat was sunk')
-		}
-	}
+function classDecorator(constructor: typeof Boat) {
+	console.log(constructor)
 }
 
-new Boat().pilot()
+function parameterDecorator(target: Boat, key: string, index: number): void {
+	console.log(key, index)
+}
+
+function testDecorator(target: any, key: string ): void {
+	console.log(key)
+}
+
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value
+
+    desc.value = function () {
+      try {
+        method()
+      } catch (e) {
+        console.log(errorMessage)
+      }
+    }
+  }
+}
+
+new Boat().pilot('fast', true)
