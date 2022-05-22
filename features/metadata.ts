86d509ca60947db8cb90d90778c59edd93fa5f03
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-22 22:38:38 +0200

    Introduced metadata

---
+++ features/metadata.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata'
+
+const plane = {
+	color: 'red'
+}
+
+Reflect.defineMetadata('note', 'hi there', plane, 'color')
+
+const note = Reflect.getMetadata('note', plane, 'color')
+console.log(note)
