dfdfcba293a2ed92ce380f794bdd85b3a3d8c23b
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-22 16:25:04 +0200

    Introduced decorators

---
+++ features/decorators.ts
@@ -0,0 +1,28 @@
+class Boat {
+  color: string = 'red'
+
+  get formattedColor(): string {
+    return `This boat's color is ${this.color}`
+  }
+
+	@logError
+  pilot(): void {
+		throw new Error()
+    console.log('swish')
+  }
+}
+
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value
+
+	desc.value = function () {
+		try {
+			method()
+		}
+		catch(e){
+			console.log('Oops, boat was sunk')
+		}
+	}
+}
+
+new Boat().pilot()
