1995c541017e58b7b443b9710f09fb465aef3b5f
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-23 16:03:56 +0200

    Created Router singleton

--- server/build/index.js
+++ server/build/index.js
@@ -7,13 +7,13 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_session_1 = __importDefault(require("cookie-session"));
 const express_1 = __importDefault(require("express"));
 const loginRoutes_1 = require("./routes/loginRoutes");
-const controller_1 = require("./controllers/decorators/controller");
+const AppRouter_1 = require("./AppRouter");
 require("./controllers/LoginController");
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_session_1.default)({ keys: ['haha'] }));
 app.use(loginRoutes_1.router);
-app.use(controller_1.router);
+app.use(AppRouter_1.AppRouter.getInstance());
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
