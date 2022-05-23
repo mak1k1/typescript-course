edbb52d6acc42bd161c52fde03b7e15b52a87782
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-23 16:12:02 +0200

    Defined a RouteBinder

--- server/build/controllers/decorators/controller.js
+++ server/build/controllers/decorators/controller.js
@@ -9,8 +9,9 @@ function controller(routePrefix) {
         for (let key in target.prototype) {
             const routeHandler = target.prototype[key];
             const path = Reflect.getMetadata('path', target.prototype, key);
+            const method = Reflect.getMetadata('method', target.prototype, key);
             if (path) {
-                router.get(`${routePrefix}${path}`, routeHandler);
+                router[method](`${routePrefix}${path}`, routeHandler);
             }
         }
     };
--- server/build/controllers/decorators/routes.js
+++ server/build/controllers/decorators/routes.js
@@ -1,10 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.get = void 0;
+exports.patch = exports.del = exports.post = exports.put = exports.get = void 0;
 require("reflect-metadata");
-function get(path) {
-    return function (target, key, desc) {
-        Reflect.defineMetadata('path', path, target, key);
+const Methods_1 = require("./Methods");
+function routeBinder(method) {
+    return function (path) {
+        return function (target, key, desc) {
+            Reflect.defineMetadata('path', path, target, key);
+            Reflect.defineMetadata('method', method, target, key);
+        };
     };
 }
-exports.get = get;
+exports.get = routeBinder(Methods_1.Methods.get);
+exports.put = routeBinder(Methods_1.Methods.put);
+exports.post = routeBinder(Methods_1.Methods.post);
+exports.del = routeBinder(Methods_1.Methods.del);
+exports.patch = routeBinder(Methods_1.Methods.patch);
