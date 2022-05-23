185d93d833e54cba466f37fd85b92d193b4d4e3b
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-23 16:16:56 +0200

    Created enum for metadata keys

--- server/build/controllers/decorators/controller.js
+++ server/build/controllers/decorators/controller.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
 const AppRouter_1 = require("../../AppRouter");
+const MetadataKeys_1 = require("./MetadataKeys");
 function controller(routePrefix) {
     return function (target) {
         const router = AppRouter_1.AppRouter.getInstance();
         for (let key in target.prototype) {
             const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata('path', target.prototype, key);
-            const method = Reflect.getMetadata('method', target.prototype, key);
+            const path = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.path, target.prototype, key);
+            const method = Reflect.getMetadata(MetadataKeys_1.MetadataKeys.method, target.prototype, key);
             if (path) {
                 router[method](`${routePrefix}${path}`, routeHandler);
             }
