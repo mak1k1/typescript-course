8271fa0a55463904b03f6bdb4a3f30dae43525b7
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-24 16:52:05 +0200

    Setup props interface and state setting

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component{
+interface AppProps {
+	color?: string
+}
+
+class App extends React.Component<AppProps>{
+	state = { counter: 0 }
+
+	onIncrement = (): void =>{
+		this.setState({counter: this.state.counter + 1})
+	}
+
+	onDecrement = (): void =>{
+		this.setState({counter: this.state.counter - 1})
+	}
+
 	render() {
-		return <div>Hi there</div>
+		return (
+			<div>
+				<button onClick={this.onIncrement}>Increment</button>
+				<button onClick={this.onDecrement}>Decrement</button>
+				{this.state.counter}
+			</div>
+		)
 	}
 }
 
 ReactDOM.render(
-	<App />,
+	<App color="red"/>,
 	document.querySelector('#root')
 )
