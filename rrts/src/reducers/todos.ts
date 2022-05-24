be919634f68d5a0fa55868b60f098bfc13817155
mak1k1 <martin.cehelsky.ja@gmail.com>
2022-05-24 22:59:03 +0200

    Added todo deletion and loading state

--- rrts/src/reducers/todos.ts
+++ rrts/src/reducers/todos.ts
@@ -9,9 +9,12 @@ export const todosReducer = (
   state: Todo[] = [],
   action: Action
 ) => {
+  
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload)
     default:
       return state
   }
